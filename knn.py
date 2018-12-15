b454dab14ca7d0e64faee6922389a20fb8fc1f03
Andy <yizhengf@sfu.ca>
2018-12-15 10:56:26 +0800

    compare k values in knn model

--- knn.py
+++ knn.py
@@ -1,41 +1,58 @@
-import numpy as np
-import pandas as pd
+# 人工智能数据源下载地址：https://video.mugglecode.com/data_ai.zip，下载压缩包后解压即可（数据源与上节课相同）
+# -*- coding: utf-8 -*-
 
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from sklearn import datasets
-from sklearn.decomposition import PCA
+"""
+    任务：鸢尾花识别
+"""
+import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
-iris = datasets.load_iris()
+
+import ai_utils
+
+DATA_FILE = './data_ai/Iris.csv'
+
+SPECIES_LABEL_DICT = {
+    'Iris-setosa':      0,  # 山鸢尾
+    'Iris-versicolor':  1,  # 变色鸢尾
+    'Iris-virginica':   2   # 维吉尼亚鸢尾
+}
+
+# 使用的特征列
+FEAT_COLS = ['SepalLengthCm', 'SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm']
+
+
+def investigate_knn(iris_data, sel_cols, k_val):
+    """
+        不同的K值对模型的影响
+    """
+    X = iris_data[sel_cols].values
+    y = iris_data['Label'].values
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1/3, random_state=10)
+    knn_model = KNeighborsClassifier(n_neighbors=k_val)   #特殊指定k值
+    knn_model.fit(X_train, y_train)
+    accuracy = knn_model.score(X_test, y_test)
+    print('k={}，accuracy={:.2f}%'.format(k_val, accuracy * 100))
+    y_pred = knn_model.predict(X_test)
+
+
+
 
 def main():
-    '''main function'''
+    """
+        主函数
+    """
+    # 读取数据集
+    iris_data = pd.read_csv(DATA_FILE, index_col='Id')
+    iris_data['Label'] = iris_data['Species'].map(SPECIES_LABEL_DICT)
 
-    #load data
-    iris = datasets.load_iris()
+    k_vals = [1,3, 5, 10,15,20,25]
+    sel_cols = ['SepalLengthCm', 'SepalWidthCm']
+    for k_val in k_vals:
+        investigate_knn(iris_data, sel_cols, k_val)
 
-    X = iris.data[:, :2]  # we only take the first two features.
-    y = iris.target
-
-    #split
-    X_train,X_test, y_train,y_test=train_test_split(X,y, test_size = 1/3, random_state =2018)
-    #declare model
-    knn_model = KNeighborsClassifier()
-
-    #train model
-
-    knn_model.fit(X_train,y_train)
-
-    #evaluation
-
-    accuracy = knn_model.score(X_test,y_test)
-    print('accuarcy:{:.2f}%'.format(accuracy*100))
-    idx = 20
-    test_sample_feat = [X_test[idx,:]]
-    y_true = y_test[0]
-    y_pred = knn_model.predict(test_sample_feat)
-    print('true label{}, predict label{}'.format(y_true,y_pred))
 
 if __name__ == '__main__':
     main()
+
